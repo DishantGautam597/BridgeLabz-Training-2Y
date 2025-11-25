55e44c429f3e7cb14bd896b8ca653692e62bef68
Dishant Gautam <gautamdishant76@gmail.com>
2025-11-25 10:20:07 +0530

    Add Project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import com.tech.Student_Database_Management.Main;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    Main.main(args);
+  }
+}
